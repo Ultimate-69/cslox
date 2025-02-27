e3819b192b7d3fdf92058fe2be1ddb6127767b3c
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-02-28 00:32:03 +0200

    initial files added

---
+++ cslox/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+if (args.Length < 2)
+{
+    Console.Error.WriteLine("Usage: ./your_program.sh tokenize <filename>");
+    Environment.Exit(1);
+}
+
+string command = args[0];
+string filename = args[1];
+
+if (command != "tokenize")
+{
+    Console.Error.WriteLine($"Unknown command: {command}");
+    Environment.Exit(1);
+}
+
+string fileContents = File.ReadAllText(filename);
+
+Console.Error.WriteLine("Logs from your program will appear here!");
+
+if (!string.IsNullOrEmpty(fileContents))
+{
+     foreach (char character in fileContents)
+     {
+        if (character == '(')
+        {
+            Console.WriteLine("LEFT_PAREN ( null");
+        }
+        else if (character == ')')
+        {
+            Console.WriteLine("RIGHT_PAREN ) null");
+        }
+     }
+     Console.WriteLine("EOF null");
+}
+else
+{
+     Console.WriteLine("EOF  null"); // Placeholder, remove this line when implementing the scanner
+}
